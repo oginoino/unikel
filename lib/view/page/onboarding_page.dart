ea88d43ed18ab96bf3894a8bbffbdf42b7c106cb
Gino <ginaldolaranjeiras@gmail.com>
2025-10-11 19:32:38 -0300

    add padding

--- lib/view/page/onboarding_page.dart
+++ lib/view/page/onboarding_page.dart
@@ -5,15 +5,18 @@ class OnboardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: FilledButton.icon(
-        iconAlignment: IconAlignment.end,
-        icon: Icon(Icons.arrow_forward_ios_rounded),
-        label: Text('Next'),
-        onPressed: () {
-          context.read<OnboardingProvider>().setOnboardingComplete(true);
-          context.go(Routes.home);
-        },
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: uiConstants.spacing4),
+      child: Center(
+        child: FilledButton.icon(
+          iconAlignment: IconAlignment.end,
+          icon: Icon(Icons.arrow_forward_ios_rounded),
+          label: Text('Next'),
+          onPressed: () {
+            context.read<OnboardingProvider>().setOnboardingComplete(true);
+            context.go(Routes.home);
+          },
+        ),
       ),
     );
   }
