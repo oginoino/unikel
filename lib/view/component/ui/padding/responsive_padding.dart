1fef3cd069ca815d9b5dcaa403d85d199194b19d
Gino <ginaldolaranjeiras@gmail.com>
2025-10-12 14:46:24 -0300

    padding folder

--- lib/view/component/ui/padding/responsive_padding.dart
+++ lib/view/component/ui/padding/responsive_padding.dart
@@ -1,4 +1,4 @@
-import '../../../utils/imports/common_libs.dart';
+import '../../../../utils/imports/common_libs.dart';
 
 class ResponsivePadding extends StatelessWidget {
   const ResponsivePadding({super.key, required this.child});
