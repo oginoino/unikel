bc664462f48197300d5fe114fbe5fb14a1644fa2
Gino <ginaldolaranjeiras@gmail.com>
2025-10-13 02:27:39 -0300

    imporve space

--- lib/view/component/ui/screen/cta_to_register_screen.dart
+++ lib/view/component/ui/screen/cta_to_register_screen.dart
@@ -13,6 +13,7 @@ class CtaToRegisterScreen extends StatelessWidget {
         child: Column(
           spacing: uiConstants.spacing4,
           children: [
+            SizedBox(height: uiConstants.spacing4),
             SizedBox(
               height: MediaQuery.sizeOf(context).height * 0.5,
               child: Text(
